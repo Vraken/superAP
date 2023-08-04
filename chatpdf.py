73890fce936bd11962bb2203eb638446532b4194
Vraken <florian.deruette@hotmail.fr>
2023-08-04 21:06:19 +0200

    Ajout lien pour télécharger les sources

--- chatpdf.py
+++ chatpdf.py
@@ -1,45 +1,44 @@
 import streamlit as st
 import requests
+from sourceInfo import sourceInfo
+import random
 
 st.title("Super AP")
+guideReg = sourceInfo("Guide réglementaire du scoutisme", "d4d373cf-4da4-4420-9038-9956e2cac86d","https://ressources.sgdf.fr/public/download/119/","resources/guide_reg.png")
+Balise = sourceInfo("Balises", "10c283bf-524c-4553-9681-83934b8c9cbd","https://chefscadres.sgdf.fr/ressources/#/explore/tag/1101","resources/balise.png")
+GPS = sourceInfo("GPS", "0d7d338b-4415-4772-99f2-31a98326f5bb","https://ressources.sgdf.fr/public/download/1575/","resources/GPS.png")
+pdfHygieneAcm = sourceInfo("Guide des bonnes pratiques de l'hygiène de la restauration en ACM", "61488718-29cc-4cd7-a9e7-89465dce0241","https://ressources.sgdf.fr/public/download/1896/")
 
-# Labels
-labelGuideReg = "Guide réglementaire du scoutisme"
-labelBalise = "Balises"
-labelGPS = "GPS"
-labelHygieneAcm="Guide des bonnes pratiques de l'hygiène de la restauration en ACM"
+listSourceInfo = [guideReg, Balise, GPS, pdfHygieneAcm]
+labels = [sourceInfo.label for sourceInfo in listSourceInfo]
 
-#Refs des pdf
-pdfGuideReg = "d4d373cf-4da4-4420-9038-9956e2cac86d"
-pdfBalises = "10c283bf-524c-4553-9681-83934b8c9cbd"
-pdfGps = "0d7d338b-4415-4772-99f2-31a98326f5bb"
-pdfHygieneAcm= "61488718-29cc-4cd7-a9e7-89465dce0241"
-
-listSourceInfo = [labelGuideReg, labelGPS, labelBalise,labelHygieneAcm]
-sourceInfo = st.multiselect("Interlocuteurs", listSourceInfo, listSourceInfo)
-
+sourceInfoLabel = st.multiselect("Interlocuteurs", labels, labels)
 
 question = st.chat_input("Poser une question")
 session = requests.Session()
+
+user_agent_list = [
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.1 Safari/605.1.15',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:77.0) Gecko/20100101 Firefox/77.0',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:77.0) Gecko/20100101 Firefox/77.0',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36',
+]
+
 session.headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36"
+    "User-Agent": random.choice(user_agent_list)
 }
 
 st.sidebar.title("Sources")
 
+def getSourceInfoFromLabel(label) -> sourceInfo:
+    return [sourceInfo for sourceInfo in listSourceInfo if sourceInfo.label == label][0]
 
-def askQuestion(question, sourceInfo):
-    uuidPdf = pdfGuideReg
-    if sourceInfo == labelGuideReg:
-        uuidPdf = pdfGuideReg
-    elif sourceInfo == labelGPS:
-        uuidPdf = pdfGps
-    elif sourceInfo == labelBalise:
-        uuidPdf = pdfBalises
-    elif sourceInfo == labelHygieneAcm:
-        uuidPdf = pdfHygieneAcm
 
-    with st.spinner(text=sourceInfo + " est en train d'écrire ..."):
+def askQuestion(question, sourceInfoLabel):
+    sourceInfo = getSourceInfoFromLabel(sourceInfoLabel)
+
+    with st.spinner(text=sourceInfo.label + " est en train d'écrire ..."):
         response = session.post("https://reederproduction.uk.r.appspot.com/querycollection",
                                 headers={
                                     "authority": "reederproduction.uk.r.appspot.com",
@@ -51,16 +50,13 @@ def askQuestion(question, sourceInfo):
                                     "accept-language": "fr-FR,fr;q=0.7",
                                     "origin": "https://reeder.ai",
                                     "referer": "https://reeder.ai/",
-                                    "sec-ch-ua": "\"Not/A)Brand\";v=\"99\", \"Brave\";v=\"115\", \"Chromium\";v=\"115\"",
-                                    "sec-ch-ua-mobile": "?0",
-                                    "sec-ch-ua-platform": "\"Windows\"",
                                     "sec-fetch-dest": "empty",
                                     "sec-fetch-mode": "cors",
                                     "sec-fetch-site": "cross-site",
                                     "sec-gpc": "1"
                                 },
                                 json={
-                                    "collectionuuid": uuidPdf,
+                                    "collectionuuid": str(sourceInfo.refId),
                                     "question": question
                                 }
                                 )
@@ -72,11 +68,13 @@ def askQuestion(question, sourceInfo):
         # st.subheader("Source : "+sourceInfo)
         completeAnswer = answer['data']['answer']
         # st.write(completeAnswer)
-        with st.chat_message(sourceInfo, avatar="📖"):
-            st.markdown(f"**{sourceInfo}**")
+        # avatar = sourceInfo.avatar if sourceInfo.avatar!=0 else "📖"
+        with st.chat_message(sourceInfo.label, avatar=sourceInfo.avatar):
+            st.markdown(f"**{sourceInfo.label}**")
             st.write(completeAnswer)
 
-        st.sidebar.subheader(sourceInfo)
+        st.sidebar.subheader(sourceInfo.label)
+        st.sidebar.write(f"[Télécharger]({sourceInfo.link})")
         for i in range(min(len(answer['data']['sources']), 3)):
             source = answer['data']['sources'][i]
             with st.sidebar.expander("Page " + str(source['page_number'])):
@@ -86,11 +84,11 @@ def askQuestion(question, sourceInfo):
 
     else:
         st.error("Une erreur s'est produite lors de l'appel à l'API Reeder.")
-
+        print(response)
 
 if question:
     with st.chat_message("user"):
         st.write(question)
 
-    for source in sourceInfo:
+    for source in sourceInfoLabel:
         askQuestion(question, source)
---
+++ sourceInfo.py
@@ -0,0 +1,22 @@
+class sourceInfo:
+    def __init__(self, label, refId, link=0, avatar="📖"):
+        self.__label = label
+        self.__refId = refId
+        self.__link = link
+        self.__avatar = avatar
+
+    @property
+    def label(self):
+        return self.__label
+
+    @property
+    def refId(self):
+        return self.__refId
+
+    @property
+    def link(self):
+        return self.__link
+
+    @property
+    def avatar(self):
+        return self.__avatar
